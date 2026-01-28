f93667cc375e6a41f046870c78bc7404aca957dd
ljj <2422301985@qq.com>
2026-01-28 21:29:00 +0800

    添加物体交互

---
+++ Assets/Script/GameManagerr.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 单例实例
+    public static GameManager Instance { get; private set; }
+
+    [Header("UI References")]
+    [SerializeField] private CanvasGroup globalBlocker; // 拖入带 CanvasGroup 的全屏遮罩
+
+    private void Awake()
+    {
+        // 确保场景中只有一个 GameManager
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        
+        // 初始状态：关闭遮罩
+        SetBlocker(false);
+        
+    }
+
+    // 提供一个统一的接口来控制
+    public void SetBlocker(bool state)
+    {
+        if (globalBlocker == null) return;
+        Debug.Log("SetBlocker: " + state);
+        // alpha=1 显示，alpha=0 隐藏
+        globalBlocker.alpha = state ? 0.5f : 0f;
+        
+        // blocksRaycasts 是核心：为 true 时拦截点击，为 false 时点击穿透
+        globalBlocker.blocksRaycasts = state;
+        
+        // 可选：是否让遮罩内的按钮失效
+        globalBlocker.interactable = state;
+        
+        
+    }
+}
---
+++ Assets/Script/GameState.cs
@@ -0,0 +1,4 @@
+﻿public static class GameState
+{
+    public static bool IsInDetailView = false; // 锁：是否正在查看详情
+}
