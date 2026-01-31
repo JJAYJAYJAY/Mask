a9c8ae98ca2fa27c99745d807a82c33100ba2147
JAYJAY <2422301985@qq.com>
2026-02-01 00:48:19 +0800

    高光bug

--- Assets/Script/PuzzleControl/HightlightControl.cs
+++ Assets/Script/PuzzleControl/HightlightControl.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 public class HightlightControl: MonoBehaviour
 {
+    public GameObject Highlight;
+
+    private void Awake()
+    {
+        Highlight = GameObject.Find("Highlight");
+    }
+
     void Start()
     {
         GameManager.Instance.Data.OnHighlightKeyItemsChanged += OnHighlightKeyItemsChanged;
