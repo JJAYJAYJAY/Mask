48a51fc1e244b01292656c384ed6dc40c6e3fa68
JAYJAY <2422301985@qq.com>
2026-02-01 11:24:42 +0800

    故事开始和结局

--- Assets/Script/Story/StoryData.cs
+++ Assets/Script/Story/StoryData.cs
@@ -9,3 +9,14 @@ public class MskStoryData: ScriptableObject
     [TextArea(5, 20)]
     public string content;
 }
+
+[CreateAssetMenu(
+    fileName = "StoryData",
+    menuName = "Game/Story Data"
+)]
+public class StroyData:ScriptableObject
+{
+    public int id;
+    [TextArea(5, 20)]
+    public string content;
+}
